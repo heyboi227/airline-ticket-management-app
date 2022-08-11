5c419ab68864aab063dcc928bb346a8f972cda2b
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2022-08-11 10:49:35 +0200

    Implemented auth controller component; Implemented administrator service, model and dto components

--- 03-back-end/src/common/IApplicationResources.interface.ts
+++ 03-back-end/src/common/IApplicationResources.interface.ts
@@ -1,6 +1,8 @@
 import * as mysql2 from "mysql2/promise";
+import AdministratorService from "../components/administrator/AdministratorService.service";
 
 export interface IServices {
+  administrator: AdministratorService;
   /* TODO: implement DB entity services */
 }
 
