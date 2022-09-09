361382e7cd4eb4f0263b449d507a87624ddf188a
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2022-09-09 13:59:01 +0200

    Updated SQL dump file

--- 02-resources/diplomski.sql
+++ 02-resources/diplomski.sql
@@ -2,7 +2,7 @@
 -- Host:                         127.0.0.1
 -- Server version:               10.6.7-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
--- HeidiSQL Version:             12.0.0.6468
+-- HeidiSQL Version:             12.1.0.6537
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -33,6 +33,7 @@ CREATE TABLE IF NOT EXISTS `address` (
   `is_active` tinyint(1) unsigned NOT NULL DEFAULT 1,
   PRIMARY KEY (`address_id`),
   UNIQUE KEY `uq_address_phone_number` (`phone_number`),
+  UNIQUE KEY `uq_address_street_and_number` (`street_and_number`),
   KEY `fk_address_user_id` (`user_id`),
   KEY `fk_address_country_id` (`country_id`),
   CONSTRAINT `fk_address_country_id` FOREIGN KEY (`country_id`) REFERENCES `country` (`country_id`) ON UPDATE CASCADE,
@@ -59,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `administrator` (
 DROP TABLE IF EXISTS `aircraft`;
 CREATE TABLE IF NOT EXISTS `aircraft` (
   `aircraft_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `type` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` enum('Narrow-body','Wide-body') COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`aircraft_id`),
   UNIQUE KEY `uq_aircraft_name` (`name`)
@@ -138,25 +139,8 @@ DROP TABLE IF EXISTS `flight`;
 CREATE TABLE IF NOT EXISTS `flight` (
   `flight_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `flight_fare_code` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `price` decimal(10,2) unsigned NOT NULL,
   PRIMARY KEY (`flight_id`),
   UNIQUE KEY `uq_flight_flight_fare_code` (`flight_fare_code`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Data exporting was unselected.
-
--- Dumping structure for table diplomski_app.flight_bag
-DROP TABLE IF EXISTS `flight_bag`;
-CREATE TABLE IF NOT EXISTS `flight_bag` (
-  `flight_bag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `flight_id` int(10) unsigned NOT NULL,
-  `bag_id` int(10) unsigned NOT NULL,
-  `price` decimal(10,0) unsigned NOT NULL,
-  PRIMARY KEY (`flight_bag_id`),
-  UNIQUE KEY `uq_flight_bag_id_flight_id_bag_id` (`flight_id`,`bag_id`),
-  KEY `fk_flight_bag_bag_bag_id` (`bag_id`),
-  CONSTRAINT `fk_flight_bag_bag_bag_id` FOREIGN KEY (`bag_id`) REFERENCES `bag` (`bag_id`) ON UPDATE CASCADE,
-  CONSTRAINT `fk_flight_bag_flight_flight_id` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
@@ -186,7 +170,6 @@ CREATE TABLE IF NOT EXISTS `flight_leg` (
   `departure_date_and_time` datetime NOT NULL,
   `arrival_date_and_time` datetime NOT NULL,
   `aircraft_id` int(10) unsigned NOT NULL,
-  `price` decimal(10,2) unsigned NOT NULL,
   PRIMARY KEY (`flight_leg_id`) USING BTREE,
   UNIQUE KEY `uq_flight_flight_code` (`flight_code`),
   KEY `fk_flight_airport_origin_airport_id` (`origin_airport_id`),
@@ -195,6 +178,22 @@ CREATE TABLE IF NOT EXISTS `flight_leg` (
   CONSTRAINT `fk_flight_aircraft_aircraft_id` FOREIGN KEY (`aircraft_id`) REFERENCES `aircraft` (`aircraft_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_flight_airport_destination_airport_id` FOREIGN KEY (`destination_airport_id`) REFERENCES `airport` (`airport_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_flight_airport_origin_airport_id` FOREIGN KEY (`origin_airport_id`) REFERENCES `airport` (`airport_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table diplomski_app.flight_leg_bag
+DROP TABLE IF EXISTS `flight_leg_bag`;
+CREATE TABLE IF NOT EXISTS `flight_leg_bag` (
+  `flight_bag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `flight_id` int(10) unsigned NOT NULL,
+  `bag_id` int(10) unsigned NOT NULL,
+  `price` decimal(10,0) unsigned NOT NULL,
+  PRIMARY KEY (`flight_bag_id`),
+  UNIQUE KEY `uq_flight_bag_id_flight_id_bag_id` (`flight_id`,`bag_id`),
+  KEY `fk_flight_leg_bag_bag_id` (`bag_id`),
+  CONSTRAINT `fk_flight_leg_bag_bag_id` FOREIGN KEY (`bag_id`) REFERENCES `bag` (`bag_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_flight_leg_bag_flight_leg_id` FOREIGN KEY (`flight_id`) REFERENCES `flight_leg` (`flight_leg_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Data exporting was unselected.
@@ -220,17 +219,17 @@ DROP TABLE IF EXISTS `ticket`;
 CREATE TABLE IF NOT EXISTS `ticket` (
   `ticket_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `ticket_number` int(10) unsigned NOT NULL,
-  `ticket_holder_forename` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ticket_holder_surname` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ticket_holder_name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
   `document_id` int(10) unsigned NOT NULL,
   `price` decimal(10,2) unsigned NOT NULL,
   `user_id` int(10) unsigned NOT NULL,
   `flight_id` int(10) unsigned NOT NULL,
+  `seat_number` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`ticket_id`) USING BTREE,
   UNIQUE KEY `uq_ticket_ticket_number` (`ticket_number`),
+  UNIQUE KEY `uq_ticket_flight_id_seat_number` (`flight_id`,`seat_number`),
   KEY `fk_ticket_document_ticket_holder_document_id` (`document_id`) USING BTREE,
   KEY `fk_ticket_user_user_id` (`user_id`),
-  KEY `fk_ticket_flight_flight_id` (`flight_id`),
   CONSTRAINT `fk_ticket_document_document_id` FOREIGN KEY (`document_id`) REFERENCES `document` (`document_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_ticket_flight_flight_id` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_ticket_user_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
