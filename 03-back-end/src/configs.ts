4204207b454c0f0fee96cf7d31bb5ac2f1d92b6c
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2022-10-06 18:45:57 +0200

    Initialized document router

--- 03-back-end/src/configs.ts
+++ 03-back-end/src/configs.ts
@@ -11,6 +11,7 @@ import BagRouter from "./components/bag/BagRouter.Router";
 import TravelClassRouter from "./components/travel_class/TravelClassRouter.Router";
 import AirportRouter from "./components/airport/AirportRouter.router";
 import TicketRouter from "./components/ticket/TicketRouter.Router";
+import DocumentRouter from "./components/document/DocumentRouter.Router";
 
 const DevConfig: IConfig = {
   server: {
@@ -42,6 +43,7 @@ const DevConfig: IConfig = {
     new AuthRouter(),
     new BagRouter(),
     new CountryRouter(),
+    new DocumentRouter(),
     new TicketRouter(),
     new TravelClassRouter(),
     new UserRouter(),
