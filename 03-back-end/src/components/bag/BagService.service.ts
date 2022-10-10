daf8477b44cd13907e5e3a9905e091b53edbfca5
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2022-10-10 11:52:55 +0200

    Interfaces and methods to obtain data from linking tables

--- 03-back-end/src/components/bag/BagService.service.ts
+++ 03-back-end/src/components/bag/BagService.service.ts
@@ -4,6 +4,12 @@ import BagModel from "./BagModel.model";
 
 export interface IBagAdapterOptions extends IAdapterOptions {}
 
+interface FlightLegBagInterface {
+  flight_leg_bag_id: number;
+  flight_leg_id: number;
+  bag_id: number;
+}
+
 export default class BagService extends BaseService<
   BagModel,
   IBagAdapterOptions
@@ -22,5 +28,33 @@ export default class BagService extends BaseService<
     bag.name = data?.name;
 
     return bag;
+  }
+
+  public async getAllByFlightLegId(
+    flightLegId: number,
+    options: IBagAdapterOptions
+  ): Promise<BagModel[]> {
+    return new Promise((resolve, reject) => {
+      this.getAllFromTableByFieldNameAndValue<FlightLegBagInterface>(
+        "flight_leg_bag",
+        "flight_leg_id",
+        flightLegId
+      )
+        .then(async (result) => {
+          const bagIds = result.map((flb) => flb.bag_id);
+
+          const bags: BagModel[] = [];
+
+          for (let bagId of bagIds) {
+            const bag = await this.getById(bagId, options);
+            bags.push(bag);
+          }
+
+          resolve(bags);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
   }
 }
--- 03-back-end/src/components/travel_class/TravelClassService.service.ts
+++ 03-back-end/src/components/travel_class/TravelClassService.service.ts
@@ -4,6 +4,12 @@ import TravelClassModel from "./TravelClassModel.model";
 
 export interface ITravelClassAdapterOptions extends IAdapterOptions {}
 
+interface FlightLegTravelClassInterface {
+  flight_leg_travel_class_id: number;
+  flight_leg_id: number;
+  travel_class_id: number;
+}
+
 export default class TravelClassService extends BaseService<
   TravelClassModel,
   ITravelClassAdapterOptions
@@ -22,5 +28,33 @@ export default class TravelClassService extends BaseService<
     travelClass.name = data?.name;
 
     return travelClass;
+  }
+
+  public async getAllByFlightLegId(
+    flightLegId: number,
+    options: ITravelClassAdapterOptions
+  ): Promise<TravelClassModel[]> {
+    return new Promise((resolve, reject) => {
+      this.getAllFromTableByFieldNameAndValue<FlightLegTravelClassInterface>(
+        "flight_leg_travel_class",
+        "flight_leg_id",
+        flightLegId
+      )
+        .then(async (result) => {
+          const travelClassIds = result.map((fltc) => fltc.travel_class_id);
+
+          const travelClasss: TravelClassModel[] = [];
+
+          for (let travelClassId of travelClassIds) {
+            const travelClass = await this.getById(travelClassId, options);
+            travelClasss.push(travelClass);
+          }
+
+          resolve(travelClasss);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
   }
 }
