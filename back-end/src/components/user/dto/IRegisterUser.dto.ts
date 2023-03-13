8c4b5b27b0d4dd7d82c52d49d8912d488e4b5edd
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2023-03-13 13:55:43 +0100

    Unnecessary comments removed

--- back-end/src/components/user/dto/IRegisterUser.dto.ts
+++ back-end/src/components/user/dto/IRegisterUser.dto.ts
@@ -1,57 +1,49 @@
 import Ajv from "ajv";
 import addFormats from "ajv-formats";
-// import * as formats from "ajv-formats-draft2019/formats"; // Za IDN podrsku: npm i ajv-formats-draft2019
 import IServiceData from "../../../common/IServiceData.interface";
 
-// const ajv = new Ajv({ formats }); // Za IDN podrsku
 const ajv = new Ajv();
 addFormats(ajv);
 
 export interface IRegisterUserDto {
-    email: string;
-    password: string;
-    forename: string;
-    surname: string;
+  email: string;
+  password: string;
+  forename: string;
+  surname: string;
 }
 
 export interface IAddUser extends IServiceData {
-    email: string;
-    password_hash: string;
-    forename: string;
-    surname: string;
-    activation_code: string;
+  email: string;
+  password_hash: string;
+  forename: string;
+  surname: string;
+  activation_code: string;
 }
 
 const RegisterUserValidator = ajv.compile({
-    type: "object",
-    properties: {
-        email: {
-            type: "string",
-            // format: "idn-email", // Za IDN podrsku
-            format: "email",
-        },
-        password: {
-            type: "string",
-            pattern: "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,}$",
-        },
-        forename: {
-            type: "string",
-            minLength: 2,
-            maxLength: 64,
-        },
-        surname: {
-            type: "string",
-            minLength: 2,
-            maxLength: 64,
-        },
+  type: "object",
+  properties: {
+    email: {
+      type: "string",
+      format: "email",
     },
-    required: [
-        "email",
-        "password",
-        "forename",
-        "surname",
-    ],
-    additionalProperties: false,
+    password: {
+      type: "string",
+      pattern: "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,}$",
+    },
+    forename: {
+      type: "string",
+      minLength: 2,
+      maxLength: 64,
+    },
+    surname: {
+      type: "string",
+      minLength: 2,
+      maxLength: 64,
+    },
+  },
+  required: ["email", "password", "forename", "surname"],
+  additionalProperties: false,
 });
 
 export { RegisterUserValidator };
