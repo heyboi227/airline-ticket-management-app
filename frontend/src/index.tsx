3a1e863307508fdc519db95cca2a6f70e2b29820
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2023-03-15 15:19:02 +0100

    Custom color and font profiling

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/scss/bootstrap.scss";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
+import "./scss/custom.scss";
 import reportWebVitals from "./common/reportWebVitals";
 
 const root = ReactDOM.createRoot(
