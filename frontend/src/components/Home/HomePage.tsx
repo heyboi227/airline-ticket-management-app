f004480d99bc455acb8092d8319906feec7601ca
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2023-03-15 15:19:32 +0100

    Introductory text for the airline, to be later placed in a containing box

--- frontend/src/components/Home/HomePage.tsx
+++ frontend/src/components/Home/HomePage.tsx
@@ -1,14 +1,13 @@
 export default function HomePage() {
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center">
-      <div className="d-flex flex-column justify-content-center align-items-center mt-5">
-        <h1>Welcome to Flagalica!</h1>
-        <div className="d-flex flex-column justify-content-center align-items-center mt-3">
-          <h4>
-            Click the Play! button in the upper left portion of the menu to
-            start the quiz!
-          </h4>
-          <h4>We hope you will have a wonderful time! Enjoy!</h4>
+    <div className="w-50 d-flex flex-column justify-content-start align-items-center">
+      <div className="d-flex flex-column justify-content-start align-items-center mt-5">
+        <h1>May your next flight fulfill your expectations.</h1>
+        <div className="d-flex flex-column justify-content-start align-items-center mt-3">
+          <h2 className="h4">
+            With our near-impeccable service, that certainly would not pose a
+            problem.
+          </h2>
         </div>
       </div>
     </div>
