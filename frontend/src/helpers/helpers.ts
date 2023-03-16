b511de23290e5c56e3ea5b99d7584f00851461cc
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2023-03-16 16:56:11 +0100

    Added helper for depicting current year

--- frontend/src/helpers/helpers.ts
+++ frontend/src/helpers/helpers.ts
@@ -2,3 +2,7 @@ export function localDateFormat(date: string): string {
   const dateInstance = new Date(date);
   return dateInstance.toLocaleDateString();
 }
+
+export function getYear(): number {
+  return new Date().getFullYear();
+}
