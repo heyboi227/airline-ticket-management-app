3d7bf782c01556e529cc67afeab7c31c2b2e774d
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2023-03-16 16:55:40 +0100

    Home page enriched with background image and introductory text

--- frontend/src/components/Home/HomePage.tsx
+++ frontend/src/components/Home/HomePage.tsx
@@ -1,13 +1,17 @@
+import "./HomePage.scss";
+
 export default function HomePage() {
   return (
-    <div className="w-50 d-flex flex-column justify-content-start align-items-center">
-      <div className="d-flex flex-column justify-content-start align-items-center mt-5">
-        <h1>May your next flight fulfill your expectations.</h1>
-        <div className="d-flex flex-column justify-content-start align-items-center mt-3">
-          <h2 className="h4">
-            With our near-impeccable service, that certainly would not pose a
-            problem.
-          </h2>
+    <div className="landing-page-header">
+      <div className="w-50 d-flex flex-column justify-content-start align-items-center">
+        <div className="d-flex flex-column justify-content-start align-items-center ms-5 intro-text">
+          <h1>May your next flight fulfill your expectations.</h1>
+          <div className="d-flex flex-column justify-content-start align-items-center mt-3">
+            <h2>
+              With our near-impeccable service, that certainly would not pose a
+              problem.
+            </h2>
+          </div>
         </div>
       </div>
     </div>
