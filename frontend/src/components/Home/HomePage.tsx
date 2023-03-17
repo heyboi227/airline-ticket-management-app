31e7d3d461290ca1ddbe54ea1ddbc6a6b021a4db
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2023-03-17 17:29:25 +0100

    Home page text redesign; Search integration

--- frontend/src/components/Home/HomePage.tsx
+++ frontend/src/components/Home/HomePage.tsx
@@ -1,19 +1,23 @@
+import Search from "../Search/Search";
 import "./HomePage.scss";
 
 export default function HomePage() {
   return (
-    <div className="landing-page-header">
-      <div className="w-50 d-flex flex-column justify-content-start align-items-center">
-        <div className="d-flex flex-column justify-content-start align-items-center ms-5 intro-text">
-          <h1>May your next flight fulfill your expectations.</h1>
-          <div className="d-flex flex-column justify-content-start align-items-center mt-3">
-            <h2>
-              With our near-impeccable service, that certainly would not pose a
-              problem.
-            </h2>
+    <>
+      <div className="landing-page-header">
+        <div className="w-50 d-flex flex-column justify-content-start align-items-center">
+          <div className="d-flex flex-column justify-content-start align-items-center ms-5 intro-text">
+            <h1>Fly with confidence and comfort.</h1>
+            <div className="d-flex flex-column justify-content-start align-items-center mt-3">
+              <h2>
+                With our near-impeccable service, there will not be any lacking
+                of these two qualities.
+              </h2>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Search />
+    </>
   );
 }
