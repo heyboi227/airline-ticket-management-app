db2bf9bd18e041c711fed220fa34dd122b30cfdf
Milos Jeknic <48435822+heyboi227@users.noreply.github.com>
2023-05-28 17:48:04 +0200

    Travel class prices could now be added in the flight add form

--- frontend/src/components/Administrator/Dashboard/AdminFlightAdd.tsx
+++ frontend/src/components/Administrator/Dashboard/AdminFlightAdd.tsx
@@ -2,13 +2,18 @@ import { useEffect, useReducer, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { api } from "../../../api/api";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSave } from "@fortawesome/free-regular-svg-icons";
+import {
+  faCheckSquare,
+  faSave,
+  faSquare,
+} from "@fortawesome/free-regular-svg-icons";
 import IAirport from "../../../models/IAirport.model";
 import IAircraft from "../../../models/IAircraft.model";
 import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { srLatn } from "date-fns/locale";
 import { parseISO } from "date-fns";
+import ITravelClass from "../../../models/ITravelClass.model";
 
 interface IAddFlightFormState {
   flightCode: string;
@@ -17,6 +22,10 @@ interface IAddFlightFormState {
   departureDateAndTime: string;
   arrivalDateAndTime: string;
   aircraftId: number;
+  travelClasses: {
+    travelClassId: number;
+    price: number;
+  }[];
 }
 
 type TSetFlightCode = { type: "addFlightForm/setFlightCode"; value: string };
@@ -37,6 +46,15 @@ type TSetArrivalDateAndTime = {
   value: string;
 };
 type TSetAircraftId = { type: "addFlightForm/setAircraftId"; value: number };
+type TAddTravelClass = { type: "addFlightForm/addTravelClass"; value: number };
+type TRemoveTravelClass = {
+  type: "addFlightForm/removeTravelClass";
+  value: number;
+};
+type TSetTravelClassPrice = {
+  type: "addFlightForm/setTravelClassPrice";
+  value: { travelClassId: number; price: number };
+};
 
 type AddFlightFormAction =
   | TSetFlightCode
@@ -44,7 +62,10 @@ type AddFlightFormAction =
   | TSetDestinationAirportId
   | TSetDepartureDateAndTime
   | TSetArrivalDateAndTime
-  | TSetAircraftId;
+  | TSetAircraftId
+  | TAddTravelClass
+  | TRemoveTravelClass
+  | TSetTravelClassPrice;
 
 function AddFlightFormReducer(
   oldState: IAddFlightFormState,
@@ -93,6 +114,76 @@ function AddFlightFormReducer(
       };
     }
 
+    case "addFlightForm/addTravelClass": {
+      if (
+        oldState.travelClasses.find(
+          (travelClass) => travelClass.travelClassId === action.value
+        )
+      ) {
+        return oldState;
+      }
+
+      return {
+        ...oldState,
+        travelClasses: [
+          ...oldState.travelClasses.map((travelClass) => {
+            return { ...travelClass };
+          }),
+          { travelClassId: action.value, price: 0 },
+        ],
+      };
+    }
+
+    case "addFlightForm/removeTravelClass": {
+      if (
+        !oldState.travelClasses.find(
+          (travelClass) => travelClass.travelClassId === action.value
+        )
+      ) {
+        return oldState;
+      }
+
+      return {
+        ...oldState,
+        travelClasses: [
+          ...oldState.travelClasses
+            .map((travelClass) => {
+              return { ...travelClass };
+            })
+            .filter(
+              (travelClass) => travelClass.travelClassId !== action.value
+            ),
+        ],
+      };
+    }
+
+    case "addFlightForm/setTravelClassPrice": {
+      if (
+        !oldState.travelClasses.find(
+          (travelClass) =>
+            travelClass.travelClassId === action.value.travelClassId
+        )
+      ) {
+        return oldState;
+      }
+
+      return {
+        ...oldState,
+        travelClasses: [
+          ...oldState.travelClasses.map((travelClass) => {
+            if (action.value.travelClassId !== travelClass.travelClassId) {
+              return { ...travelClass };
+            }
+
+            return {
+              ...travelClass,
+              price: action.value.price,
+            };
+          }),
+        ],
+      };
+    }
+
     default:
       return oldState;
   }
@@ -102,6 +193,7 @@ export default function AdminFlightAdd() {
   const [errorMessage, setErrorMessage] = useState<string>("");
   const [airports, setAirports] = useState<IAirport[]>([]);
   const [aircraft, setAircraft] = useState<IAircraft[]>([]);
+  const [travelClasses, setTravelClasses] = useState<ITravelClass[]>([]);
 
   const navigate = useNavigate();
 
@@ -114,6 +206,7 @@ export default function AdminFlightAdd() {
       departureDateAndTime: "",
       arrivalDateAndTime: "",
       aircraftId: 0,
+      travelClasses: [],
     }
   );
 
@@ -159,18 +252,39 @@ export default function AdminFlightAdd() {
       });
   };
 
+  const fetchTravelClasses = () => {
+    api("get", "/api/travel-class", "administrator")
+      .then((res) => {
+        if (res.status !== "ok") {
+          throw new Error(
+            "Could not fetch travel classes! Reason: " +
+              res?.data
+                ?.map(
+                  (error: any) => error?.instancePath + " " + error?.message
+                )
+                .join(", ")
+          );
+        }
+
+        setTravelClasses(res.data);
+      })
+      .catch((error) => {
+        setErrorMessage(error?.message ?? "Unknown error!");
+      });
+  };
+
   useEffect(() => {
     fetchAirports();
     fetchAircraft();
+    fetchTravelClasses();
   }, []);
 
   const doAddFlight = () => {
-    console.log(formState);
     api("post", "/api/flight", "administrator", formState)
       .then((res) => {
         if (res.status !== "ok") {
           throw new Error(
-            "Could not add this item! Reason: " +
+            "Could not add this flight! Reason: " +
               res?.data
                 ?.map(
                   (error: any) => error?.instancePath + " " + error?.message
@@ -342,6 +456,75 @@ export default function AdminFlightAdd() {
                   ))}
                 </select>
               </div>
+            </div>
+
+            <div className="form-group mb-3">
+              <label>Travel classes</label>
+
+              {travelClasses?.map((travelClass) => {
+                const travelClassData = formState.travelClasses.find(
+                  (tc) => tc.travelClassId === travelClass.travelClassId
+                );
+
+                return (
+                  <div
+                    className="row"
+                    key={"travel-class-" + travelClass.travelClassId}
+                  >
+                    <div className="col col-3">
+                      {travelClassData ? (
+                        <FontAwesomeIcon
+                          onClick={() =>
+                            dispatchFormStateAction({
+                              type: "addFlightForm/removeTravelClass",
+                              value: travelClass.travelClassId,
+                            })
+                          }
+                          icon={faCheckSquare}
+                        />
+                      ) : (
+                        <FontAwesomeIcon
+                          onClick={() =>
+                            dispatchFormStateAction({
+                              type: "addFlightForm/addTravelClass",
+                              value: travelClass.travelClassId,
+                            })
+                          }
+                          icon={faSquare}
+                        />
+                      )}{" "}
+                      {travelClass.travelClassSubname}
+                    </div>
+
+                    {travelClassData && (
+                      <>
+                        <div className="col col-2"></div>
+                        <div className="col col-2">
+                          <div className="input-group input-group-sm">
+                            <input
+                              type="number"
+                              min={0.01}
+                              step={0.01}
+                              value={travelClassData.price}
+                              className="form-control form-control-sm"
+                              onChange={(e) =>
+                                dispatchFormStateAction({
+                                  type: "addFlightForm/setTravelClassPrice",
+                                  value: {
+                                    travelClassId: travelClass.travelClassId,
+                                    price: +e.target.value,
+                                  },
+                                })
+                              }
+                            />
+                            <span className="input-group-text">RSD</span>
+                          </div>
+                        </div>
+                      </>
+                    )}
+                  </div>
+                );
+              })}
             </div>
 
             <div className="form-group mb-3">
